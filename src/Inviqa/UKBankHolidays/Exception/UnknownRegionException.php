82d80809bd503140c59cc6a396b2390660756a39
Benjamin Alsleben <balsleben@inviqa.com>
2019-11-22 17:14:13 +0100

    refactor region

--- src/Inviqa/UKBankHolidays/Exception/UnknownRegionException.php
+++ src/Inviqa/UKBankHolidays/Exception/UnknownRegionException.php
@@ -2,15 +2,13 @@
 
 namespace Inviqa\UKBankHolidays\Exception;
 
-use Inviqa\UKBankHolidays\Region\Region;
-
 class UnknownRegionException extends UKBankHolidaysException
 {
     public const UNKNOW_REGION_MESSAGE = 'Unknow Region: "%s"';
 
-    public static function withRegion(Region $region): UnknownRegionException
+    public static function withRegion(string $region): UnknownRegionException
     {
-        $exception = new static(sprintf(self::UNKNOW_REGION_MESSAGE, $region->getRegion()));
+        $exception = new static(sprintf(self::UNKNOW_REGION_MESSAGE, $region));
 
         return $exception;
     }
--- src/Inviqa/UKBankHolidays/Region/Region.php
+++ src/Inviqa/UKBankHolidays/Region/Region.php
@@ -2,7 +2,44 @@
 
 namespace Inviqa\UKBankHolidays\Region;
 
-interface Region
+use Inviqa\UKBankHolidays\Exception\UnknownRegionException;
+
+final class Region
 {
-    public function getRegion(): string;
+    private const REGION_ENGLAND_WALES = 'england-and-wales';
+    private const REGION_NORTHERN_IRELAND = 'northern-ireland';
+    private const REGION_SCOTLAND = 'scotland';
+
+    private $regions = [
+        self::REGION_ENGLAND_WALES,
+        self::REGION_NORTHERN_IRELAND,
+        self::REGION_SCOTLAND,
+    ];
+
+    private $region;
+
+    private function __construct()
+    {
+    }
+
+    public static function createFromString(string $region): Region
+    {
+        $regionObj = new self();
+
+        if (!$regionObj->isAvailableRegion($region)) {
+            throw UnknownRegionException::withRegion($region);
+        }
+
+        $regionObj->region = $region;
+    }
+
+    public function isAvailableRegion(string $region): bool
+    {
+        return in_array($string, $this->regions);
+    }
+
+    public function getRegion()
+    {
+        return $this->region;
+    }
 }
