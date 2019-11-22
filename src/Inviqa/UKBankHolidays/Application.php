8f22458e7002e1c3c11078ec20e5f8b9a268b001
Benjamin Alsleben <balsleben@inviqa.com>
2019-11-22 10:30:00 +0100

    add decorator

--- src/Inviqa/UKBankHolidays/Application.php
+++ src/Inviqa/UKBankHolidays/Application.php
@@ -6,19 +6,23 @@ use DateTimeInterface;
 use Inviqa\UKBankHolidays\Cache\CacheProvider;
 use Inviqa\UKBankHolidays\Region\Region;
 use Inviqa\UKBankHolidays\Service\BankHolidayService;
+use Inviqa\UKBankHolidays\Service\BankHolidayServiceDecorator;
 use Inviqa\UKBankHolidays\Service\BankHolidayServiceFactory;
 
 class Application
 {
     private const CACHE_KEY_PREFFIX_CHECK = 'check_';
     private const CACHE_KEY_PREFFIX_LIST = 'all_';
+    private const CACHE_KEY_PLACEHOLDER = '#_';
 
+    private $bankHolidayDecorator;
     private $bankHolidayService;
     private $cache;
 
     public function __construct(Configuration $configuration, CacheProvider $cacheProvider)
     {
         $this->bankHolidayService = BankHolidayServiceFactory::buildFrom($configuration);
+        $this->bankHolidayDecorator = new BankHolidayServiceDecorator($this->bankHolidayService);
         $this->cache = $cacheProvider;
     }
 
@@ -40,26 +44,27 @@ class Application
         ?DateTimeInterface $from = null,
         ?DateTimeInterface $to = null,
         ?Region $region = null
-    ): array {
+    ): array
+    {
 
         $cacheKey = self::CACHE_KEY_PREFFIX_LIST;
 
         if ($from !== null) {
             $cacheKey .= $from->getTimestamp();
         } else {
-            $cacheKey .= '#_';
+            $cacheKey .= self::CACHE_KEY_PLACEHOLDER;
         }
 
         if ($to !== null) {
             $cacheKey .= $to->getTimestamp();
         } else {
-            $cacheKey .= '#_';
+            $cacheKey .= self::CACHE_KEY_PLACEHOLDER;
         }
 
         if ($region !== null) {
             $cacheKey .= $region->getRegion();
         } else {
-            $cacheKey .= '#';
+            $cacheKey .= self::CACHE_KEY_PLACEHOLDER;
         }
 
 
---
+++ src/Inviqa/UKBankHolidays/Service/BankHolidayServiceDecorator.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Inviqa\UKBankHolidays\Service;
+
+use DateTimeInterface;
+use Exception;
+use Inviqa\UKBankHolidays\Client\Client;
+use Inviqa\UKBankHolidays\Exception\UKBankHolidaysException;
+use Inviqa\UKBankHolidays\Region\Region;
+use Inviqa\UKBankHolidays\ResponseParser;
+use Inviqa\UKBankHolidays\Result;
+
+class BankHolidayServiceDecorator
+{
+    private $bankHolidayService;
+
+    public function __construct(BankHolidayService $bankHolidayService)
+    {
+        $this->bankHolidayService = $bankHolidayService;
+    }
+
+    public function check(DateTimeInterface $dateTime): bool
+    {
+        $result = $this->getBankHolidays();
+
+        return true;
+    }
+
+    public function getAll(
+        ?DateTimeInterface $from = null,
+        ?DateTimeInterface $to = null,
+        ?Region $region = null
+    ): array {
+        $result = $this->getBankHolidays();
+
+        return [];
+    }
+
+    private function getBankHolidays(): Result
+    {
+        try {
+            $responseBody = $this->apiClient->getBankHolidays();
+
+            return $this->responseParser->extractResultFrom($responseBody);
+        } catch (Exception $e) {
+            throw new UKBankHolidaysException($e->getMessage(), $e->getCode(), $e);
+        }
+    }
+}
